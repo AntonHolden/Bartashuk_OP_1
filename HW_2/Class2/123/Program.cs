1e636c02c4a5cf3581e82c55ff95baaeb0d8dab8
AntonHolden <robsmt@mail.ru>
2022-09-09 18:52:14 +0300

    HW2_Task1 (Done)

---
+++ HW_2/Class2/123/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string s = "abcdab", s1 = "ab", s2 = "xxx";
+        string[] splitter = s.Split(s1);
+        try
+        {
+            string rez = String.Concat(splitter[0], s2, splitter[1]);
+        }
+        catch
+        {
+            Console.WriteLine(splitter[0]);
+        }
+    }
+}
