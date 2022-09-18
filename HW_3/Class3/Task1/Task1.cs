7cbced723cac43cffa3753e550e4f2df6a387aa8
AntonHolden <robsmt@mail.ru>
2022-09-19 02:15:25 +0300

    Made a game (HAVEN't TESTED)
    Haven't tested and formatted yet.

--- HW_3/Class3/Task1/Task1.cs
+++ HW_3/Class3/Task1/Task1.cs
@@ -17,7 +17,7 @@ internal enum Suit
 // Значение
 internal enum Rank
 {
-    _6=2,
+    _6 = 2,
     _7,
     _8,
     _9,
@@ -31,21 +31,22 @@ internal enum Rank
 // Карта
 record Card
 {
-    private Player player;
+    public Player player;
     private Suit suit;
     public Rank rank { get; private set; }
-    public Card(Player player) => this.player=player;
+    public Card(Player player) => this.player = player;
     public Card(Rank rank, Suit suit)
     {
         this.suit = suit;
         this.rank = rank;
-    };
+    }
+
 };
 
 // Тип для обозначения игрока (первый, второй)
 internal enum Player
 {
-    P1=1,
+    P1 = 1,
     P2
 }
 
@@ -53,16 +54,21 @@ namespace Task1
 {
     public class Task1
     {
-
- /*
- * Реализуйте игру "Пьяница" (в простейшем варианте, на колоде в 36 карт)
- * https://ru.wikipedia.org/wiki/%D0%9F%D1%8C%D1%8F%D0%BD%D0%B8%D1%86%D0%B0_(%D0%BA%D0%B0%D1%80%D1%82%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B8%D0%B3%D1%80%D0%B0)
- * Рука — это набор карт игрока. Карты выкладываются на стол из начала "рук" и сравниваются
- * только по значениям (масть игнорируется). При равных значениях сравниваются следующие карты.
- * Набор карт со стола перекладывается в конец руки победителя. Шестерка туза не бьёт.
- *
- * Реализация должна сопровождаться тестами.
- */
+        public static T pop<T>(ref List<T> list, int ind = 0)
+        {
+            T element = list[ind];
+            list.RemoveAt(ind);
+            return element;
+        }
+        /*
+        * Реализуйте игру "Пьяница" (в простейшем варианте, на колоде в 36 карт)
+        * https://ru.wikipedia.org/wiki/%D0%9F%D1%8C%D1%8F%D0%BD%D0%B8%D1%86%D0%B0_(%D0%BA%D0%B0%D1%80%D1%82%D0%BE%D1%87%D0%BD%D0%B0%D1%8F_%D0%B8%D0%B3%D1%80%D0%B0)
+        * Рука — это набор карт игрока. Карты выкладываются на стол из начала "рук" и сравниваются
+        * только по значениям (масть игнорируется). При равных значениях сравниваются следующие карты.
+        * Набор карт со стола перекладывается в конец руки победителя. Шестерка туза не бьёт.
+        *
+        * Реализация должна сопровождаться тестами.
+        */
 
         // Размер колоды
         internal const int DeckSize = 36;
@@ -70,41 +76,74 @@ namespace Task1
         // Возвращается null, если значения карт совпадают
         internal static Player? RoundWinner(Card card1, Card card2)
         {
-            return (card1.rank==card2.rank)?null:max;
+            return (card1.rank == card2.rank) ? null : (card1.rank > card2.rank) ? card1.player : card2.player;
         }
-        
-// Возвращает полную колоду (36 карт) в фиксированном порядке
-        internal static Deck FullDeck() 
+
+        // Возвращает полную колоду (36 карт) в фиксированном порядке
+        internal static Deck FullDeck()
         {
-            Deck deck =new Deck(36);
+            Deck deck = new Deck();
             int i = 0;
             foreach (Rank rank in Enum.GetValues(typeof(Rank)))
             {
                 foreach (Suit suit in Enum.GetValues(typeof(Suit)))
                 {
-                    deck[i] = new Card(rank, suit);
+                    deck.Add(new Card(rank, suit));
                     i++;
                 }
             }
             return deck;
         }
 
-// Раздача карт: случайное перемешивание (shuffle) и деление колоды пополам
-        internal static Dictionary<Player, Hand> Deal(Deck deck) 
+        // Раздача карт: случайное перемешивание (shuffle) и деление колоды пополам
+        internal static Dictionary<Player, Hand> Deal(Deck deck)
         {
-            throw new NotImplementedException();
+            var hands = new Dictionary<Player, Hand>();
+            var randomizer = new Random();
+            int deckCurrentLen = 36;
+            var currentPlayer = Player.P1;
+            for (int i = 0; i < 36; i++)
+            {
+                int ind = randomizer.Next(deckCurrentLen--);
+                deck[ind].player = currentPlayer;
+                hands[currentPlayer = 3 - currentPlayer].Insert(0,pop(ref deck,ind));
+            }
+            return hands;
         }
 
-// Один раунд игры (в том числе спор при равных картах).
-// Возвращается победитель раунда и набор карт, выложенных на стол.
-        internal static Tuple<Player, Table> Round(Dictionary<Player, Hand> hands) {
-            throw new NotImplementedException();
+        // Один раунд игры (в том числе спор при равных картах).
+        // Возвращается победитель раунда и набор карт, выложенных на стол.
+        internal static Tuple<Player?, Table> Round(ref Dictionary<Player, Hand> hands)
+        {
+            
+            Player? winner = null;
+            Table table = new Table();
+            while (winner==null)
+            {
+                if (!hands[Player.P1].Any() && !hands[Player.P1].Any()) return new Tuple<Player?, Table>(null, table);
+                else if (!hands[Player.P1].Any()) return new Tuple<Player?, Table>(Player.P2, table);
+                else if (!hands[Player.P2].Any()) return new Tuple<Player?, Table>(Player.P1, table);
+                table.Add(hands[Player.P1][0]);
+                table.Add(hands[Player.P2][0]);
+                winner = RoundWinner(hands[Player.P1][0], hands[Player.P2][0]);
+                hands[Player.P1].RemoveAt(0);
+                hands[Player.P2].RemoveAt(0);
+            }
+            return new Tuple<Player?, Table>(winner, table);
         }
 
-// Полный цикл игры (возвращается победивший игрок)
-// в процессе игры печатаются ходы
-        internal static Player Game(Dictionary<Player, Hand> hands) {
-            throw new NotImplementedException();
+        // Полный цикл игры (возвращается победивший игрок)
+        // в процессе игры печатаются ходы
+        internal static Player? Game(Dictionary<Player, Hand> hands)
+        {
+            var winnerTable = Round(ref hands);
+            while (hands[Player.P1].Any() && hands[Player.P1].Any())
+            {
+                if (winnerTable.Item1 == null) return null;
+                hands[(Player)winnerTable.Item1].AddRange(winnerTable.Item2);
+                winnerTable = Round(ref hands);
+            }
+            return winnerTable.Item1;
         }
 
         public static void Main(string[] args)
@@ -112,7 +151,8 @@ namespace Task1
             var deck = FullDeck();
             var hands = Deal(deck);
             var winner = Game(hands);
-            Console.WriteLine($"Победитель: {winner}");
+            if (winner == null) Console.WriteLine("Ничья!");
+            else Console.WriteLine($"Победитель: {winner}");
         }
     }
 }
